d31dbebad87553048c47c0231d125dea108872a8
JoseFrancisco93 <jfvc.93@gmail.com>
2025-05-31 11:04:06 -0500

    feat: add GetCatBreedsUseCase for fetching cat breeds from repository

---
+++ lib/domain/usecases/get_cat_breeds.dart
@@ -0,0 +1,11 @@
+import 'package:catbreeds_app/domain/entities/cat_breed.dart';
+import 'package:catbreeds_app/domain/repositories/cat_breed_repository.dart';
+
+class GetCatBreedsUseCase {
+  final CatBreedRepository repository;
+
+  GetCatBreedsUseCase(this.repository);
+
+  Future<List<CatBreed>> call({int page = 0, int limit = 10}) =>
+      repository.getBreeds(page: page, limit: limit);
+}
